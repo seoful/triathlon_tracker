65886a9c0b5a150cffa4993a3894121fb82eb58a
dmitriipolushin <27102001@bk.ru>
2022-11-04 21:35:29 +0300

    debugging

--- lib/app.dart
+++ lib/app.dart
@@ -4,20 +4,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:triathlon_tracker/core/app_theme.dart';
 import 'package:triathlon_tracker/core/s.dart';
 import 'package:triathlon_tracker/core/style.dart';
+import 'package:triathlon_tracker/presentation/onboarding/onboarding_main_screen.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    StaticS.init(context);
+    // StaticS.init(context);
 
-    final themeColor = ref.watch(styleProvider).themeColor;
-    final colors = ref.watch(styleProvider).colors;
-    final locale = ref.watch(localeProvider);
+    // final themeColor = ref.watch(styleProvider).themeColor;
+    // final colors = ref.watch(styleProvider).colors;
+    // final locale = ref.watch(localeProvider);
 
     return MaterialApp(
-      theme: AppTheme.theme(themeColor, colors),
+      // theme: AppTheme.theme(themeColor, colors),
       localizationsDelegates: [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -25,7 +26,8 @@ class App extends ConsumerWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.supportedLocales,
-      locale: locale,
+      // locale: locale,
+      home: const OnBoardingMainScreen(),
     );
   }
 }
